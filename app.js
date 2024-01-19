03458741df0209977228c62235496d6950cb0004
vivek-2102 <vivek21iya@gmail.com>
2024-01-19 21:22:00 +0530

    updating evaluation condition in app.js

--- app.js
+++ app.js
@@ -12,30 +12,19 @@ function btnpress() {
     let str = inp.value;
     let ch = str.slice(0, str.length - 1);
     inp.value = ch;
-  } else if (btn.innerText != "=") inp.value += btn.innerText;
+  } else if (btn.innerText != "=") 
+  inp.value += btn.innerText;
   else {
     let str = inp.value;
-    console.log(str);
-    if (str.includes("+")) {
-      let x = str.split("+");
-      let ans = Number(x[0]) + Number(x[1]);
-      inp.value = ans;
-    } else if (str.includes("-")) {
-      let x = str.split("-");
-      let ans = Number(x[0]) - Number(x[1]);
-      inp.value = ans;
-    } else if (str.includes("*")) {
-      let x = str.split("*");
-      let ans = Number(x[0]) * Number(x[1]);
-      inp.value = ans;
-    } else if (str.includes("/")) {
-      let x = str.split("/");
-      let ans = Number(x[0]) / Number(x[1]);
-      inp.value = ans;
-    } else if (str.includes("%")) {
-      let x = str.split("%");
-      let ans = Number(x[0]) % Number(x[1]);
-      inp.value = ans;
+    try{
+    inp.value=eval(str);}
+    catch(err)
+    {
+      inp.value="Syntax Error";
+      setTimeout(()=>{
+        inp.value="";
+      },1000);
+       
     }
   }
 }
